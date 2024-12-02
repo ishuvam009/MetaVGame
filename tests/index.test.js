5bc6809f53bc191bdda41d03c10d66a1bbf68fa0
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2024-12-02 23:50:34 +0530

    Commit

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const { describe } = require('yargs');
 
 const BACKEND_URL = "http://localhost:4000";
 
@@ -29,13 +30,18 @@ describe("Authentication", () => {
         const username = `shuvam-${Math.randm()}`;
         const password = "123456";
 
-        const response = await axios.post(`${BACKEND_URL}/api/v1/signup`);
+        const response = await axios.post(`${BACKEND_URL}/api/v1/signup`,{
+            password
+        });
         expect(response.statusCode).toBe(400);
     });
 
     test('Signin sucessced if username and password is correct.', async() => {
         const username = `shuvam${Math.random()}`;
         const password = "123456";
+
+        //assuming this username and password is never used/ not present in the atabase
+        axios.post(`${BACKEND_URL}/api/v1/signup`)
 
         const response = await axios.post(`${BACKEND_URL/api/v1/signup}`,{
             username,
@@ -44,5 +50,28 @@ describe("Authentication", () => {
 
         expect(response.statusCode).toBe(200);
         expect(response.body.token).toBeDefined();
-    })
+    });
+
+    test('Signin fails if either username or password is incorrect.', async() =>{
+        const username = `shuvam-${Math.random()}`;
+        const password = "123456";
+
+        axios.post(`${BACKEND_URL}/api/v1/signup`,{
+            username,
+            password
+        });
+
+        const response = await axios.post(`${BACKEND_URL}/api/v1/signin`,{
+            username: "Sjihbb",
+            password
+        })
+
+        expect(response.statusCode).toBe(403);
+    });
+
 });
+
+describe("User information endpoint", () => {
+
+    tset()
+})

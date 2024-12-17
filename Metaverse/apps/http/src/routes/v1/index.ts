b867a631bb6b99a287d4f178bef816875aba18de
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2024-12-18 03:42:38 +0530

    Commit

--- Metaverse/apps/http/src/routes/v1/index.ts
+++ Metaverse/apps/http/src/routes/v1/index.ts
@@ -1,15 +1,30 @@
 import { Router } from "express";
+import { adminRouter } from "./admin";
+import { userRouter } from "./user";
+import { spaceRouter } from "./space";
 
 export const router = Router();
 
-router.get('/signup', (req,res) => {
+router.post('/signup', (req,res) => {
     res.status(200).json({
         message: 'Signup Successful',
     })
 });
 
-router.get('/signin', (req,res) => {
+router.post('/signin', (req,res) => {
     res.status(200).json({
         message: 'Signin Successful.',
     })
 });
+
+router.get('/elements',(req,res) => {
+
+});
+
+router.get('/avatar',() => {
+
+});
+
+router.use('/admin',adminRouter);
+router.use('/user',userRouter);
+router.use('/space',spaceRouter);

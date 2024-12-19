add02c1fe3d9bfb9e540301fa77dadf6ce631000
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2024-12-19 23:46:12 +0530

    'Commit'

--- Metaverse/apps/http/src/routes/v1/space.ts
+++ Metaverse/apps/http/src/routes/v1/space.ts
@@ -1,3 +1,15 @@
 import { Router } from "express";
 
 export const spaceRouter = Router();
+
+spaceRouter.post('/', (req,res) => {
+
+});
+
+spaceRouter.delete('/:spaceId', (req,res) => {
+
+});
+
+spaceRouter.get('/all', (req,res) => {
+
+});

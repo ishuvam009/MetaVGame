2e7f8bfbb371fe83f378f75ed0097ce58f243cd1
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2024-12-28 23:46:02 +0530

    Commit

--- Metaverse/apps/http/src/routes/v1/index.ts
+++ Metaverse/apps/http/src/routes/v1/index.ts
@@ -5,10 +5,11 @@ import { spaceRouter } from "./space";
 import { SigninSchema } from "../../types";
 import { SignupSchema } from "../../types";
 import client from "@repo/db/client";
+import { date } from "zod";
 
 export const router = Router();
 
-router.post('/signup', (req,res) => {
+router.post('/signup', async (req,res) => {
     const parsedData = SignupSchema.safeParse(req.body)
     if(!parsedData.success){
          res.status(400).json({
@@ -18,7 +19,14 @@ router.post('/signup', (req,res) => {
     }else
 
     try {
-        
+        await client.user.create({
+            data: {
+                userName: parsedData.data.username,
+                password: parsedData.data.password,
+                role: parsedData.data.type === "admin" ? "Admin" : "User",
+            }
+        })
+
     } catch (error) {
         
     }

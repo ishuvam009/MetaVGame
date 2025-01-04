af0bed9fb78612685437beb7c9d24dea2b2c9c97
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2025-01-04 20:50:01 +0530

    Commit

--- Metaverse/apps/http/src/routes/v1/user.ts
+++ Metaverse/apps/http/src/routes/v1/user.ts
@@ -1,3 +1,18 @@
 import { Router } from "express";
+import { UpdateMetadataSchema } from "../../types";
+import client from "@repo/db/client";
 
 export const userRouter = Router();
+
+userRouter.post("/metadata", (req,res) => {
+    const parsedData = UpdateMetadataSchema.safeParse(req.body);
+
+    if(!parsedData.success){
+        res.status(400).json({
+            message: "VAlidation Failed"
+        })
+        return
+    }
+
+    client.user.update;
+})

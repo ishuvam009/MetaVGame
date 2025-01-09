7431121c6baa2a312d08a09cba0fe476cde25d39
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2025-01-09 23:56:59 +0530

    Commit

--- Metaverse/apps/http/src/routes/v1/space.ts
+++ Metaverse/apps/http/src/routes/v1/space.ts
@@ -1,9 +1,16 @@
 import { Router } from "express";
+import { CreateSpaceSchema } from "../../types";
 
 export const spaceRouter = Router();
 
 spaceRouter.post('/', (req,res) => {
-
+    const parsedData = CreateSpaceSchema.safeParse(req.body);
+    if(!parsedData.success){
+        res.status(400).json({
+            message: "Validation Failed."
+        })
+        return
+    }
 });
 
 spaceRouter.delete('/:spaceId', (req,res) => {
